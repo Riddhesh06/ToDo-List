95b20457675eb950e5dfa76eb77527b5d30e6e98
Riddhesh Khedekar <61085143+Riddhesh06@users.noreply.github.com>
2022-06-28 12:22:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+ 
+function App() {
+ return (
+   <div className="App">
+    	Hello World! A To Do List Will Go here!
+   </div>
+ );
+}
+ 
+export default App;
